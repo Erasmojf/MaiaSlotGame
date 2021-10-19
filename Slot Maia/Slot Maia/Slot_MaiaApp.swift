51d14a49b016213b5bc6246aa42908940ccbca8b
Erasmo J.F Da Silva <erasmogohiper@gmail.com>
2021-10-19 11:05:55 -0300

    CM - Let’s set up the Slot Machine iOS and macOS app project in Xcode
    Let’s set up the Slot Machine iOS and macOS
    app project in Xcode

---
+++ Slot Maia/Slot Maia/Slot_MaiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MaiaApp.swift
+//  Slot Maia
+//
+//  Created by Erasmo J.F Da Silva on 19/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MaiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Slot Maia/Slot Maia/View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slot Maia
+//
+//  Created by Erasmo J.F Da Silva on 19/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
