2a1dd6d580958d3fc5f7fe2b0d978f53a08042ae
Erasmo J.F Da Silva <erasmogohiper@gmail.com>
2021-10-21 12:04:47 -0300

    CM - Let’s develop the logic of the betting system with Swift 5
    Let’s develop the logic of the betting system
    with Swift 5

--- Slot Maia/Slot Maia/View/ContentView.swift
+++ Slot Maia/Slot Maia/View/ContentView.swift
@@ -18,6 +18,8 @@ struct ContentView: View {
     @State private var betAmount: Int = 10
     @State private var reels: Array = [0, 1, 2]
     @State private var showingInfoView: Bool = false
+    @State private var isActiveBet10: Bool = false
+    @State private var isActiveBet20: Bool = false
     
     // MARK: - FUNCTIONS
     func spinReels() {
@@ -53,7 +55,12 @@ struct ContentView: View {
     func playerLoses() {
         coins -= betAmount
     }
-    
+    func activateBet20() {
+        betAmount = 20
+    }
+    func activateBet10() {
+        betAmount = 10
+    }
     // MARK: - SPIN THE REELS
     // MARK: - CHECK THE WINNING
     
@@ -154,18 +161,21 @@ struct ContentView: View {
                     // MARK: - BET 20
                     HStack(alignment: .center, spacing: 10) {
                         Button(action: {
-                            print("Bet 20 coins")
+                            self.activateBet20()
+                            isActiveBet20 = true
+                            isActiveBet10 = false
+                            
                         }) {
                             Text("20")
                                 .fontWeight(.heavy)
-                                .foregroundColor(Color.white)
+                                .foregroundColor(isActiveBet20 ? Color("ColorYellow") : Color.white)
                                 .modifier(BetNumberModifier())
                         }
                         .modifier(BetCapsuleModifier())
                         
                         Image("gfx-casino-chips")
                             .resizable()
-                            .opacity(0)
+                            .opacity(isActiveBet20 ? 1 : 0)
                             .modifier(CasinoChipsModifier())
                         
                     }//HStack
@@ -174,15 +184,18 @@ struct ContentView: View {
                     HStack(alignment: .center, spacing: 10) {
                         Image("gfx-casino-chips")
                             .resizable()
-                            .opacity(1)
+                            .opacity(isActiveBet10 ? 1 : 0)
                             .modifier(CasinoChipsModifier())
                         
                         Button(action: {
-                            print("Bet 10 coins")
+                            self.activateBet10()
+                            isActiveBet10 = true
+                            isActiveBet20 = false
+                            
                         }) {
                             Text("10")
                                 .fontWeight(.heavy)
-                                .foregroundColor(Color.yellow)
+                                .foregroundColor(isActiveBet10 ? Color("ColorYellow") : Color.white)
                                 .modifier(BetNumberModifier())
                         }
                         .modifier(BetCapsuleModifier())
